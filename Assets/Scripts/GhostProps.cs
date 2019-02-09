416b92b3c94e78f0b38f4c00cc8104d7318910f2
MossGoblin <hugolabs@gmail.com>
2019-02-09 12:16:00 +0200

    Character Switch and Timers - READY

--- Assets/Scripts/GhostProps.cs
+++ Assets/Scripts/GhostProps.cs
@@ -10,6 +10,13 @@ public class GhostProps : MonoBehaviour
     [SerializeField] public float[] hrMoveSpeed;
     [SerializeField] public bool[] hrDoubleJump;
     [SerializeField] public Sprite[] hrSprites;
+    [SerializeField] public float[] hrMaxTimers;
+    [SerializeField] public float[] hrActiveTempo;
+    [SerializeField] public float[] hrCoolDownTempo;
+
+    [SerializeField] private float atf; // Active Time Factor
+    [SerializeField] private float cdf; // CoolDown Factor
+
 
 
     // Start is called before the first frame update
@@ -20,6 +27,9 @@ public class GhostProps : MonoBehaviour
         hrJumpFactor = new float[3] { 12f, 8f, 5f };
         hrMoveSpeed = new float[3] { 5f, 2.5f, 1.5f };
         hrDoubleJump = new bool[3] { true, false, false };
+        hrMaxTimers = new float[3] { 30f, 30f, 30f };
+        hrActiveTempo = new float[3] { 1f * atf, 1f * atf, 1f * atf };
+        hrCoolDownTempo = new float[3] { 0.5f * cdf, 0.5f * cdf, 0.5f * cdf };
     }
 
     // Update is called once per frame
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,8 +13,10 @@ public class PlayerController : MonoBehaviour
     // Components
     SpriteRenderer heroRenderer;
     Transform heroTransform;
-    [SerializeField] Transform playerBase;
+    [SerializeField] Transform[] playerBase;
     Rigidbody2D heroRigidBody;
+    [SerializeField] Text timerDisplay;
+
 
     // Hero Stats
     [SerializeField] private string heroName;
@@ -22,21 +25,33 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private bool heroDoubleJump;
     [SerializeField] private Sprite heroSprite;
     [SerializeField] private int heroState;
+    [SerializeField] private float[] ActiveTimers;
+    [SerializeField] private float[] CoolDownTimers;
+
+    [SerializeField] private float tmpJumpFactor;
 
     // Hero Position Checks
-    [SerializeField] private bool midAir;
+    [SerializeField] private bool onGround; // Is the hero touching the ground
+    [SerializeField] private bool didDoubleJump; // Did the player double jump already
+
+    // Hero Status Checks
+    [SerializeField] private bool shouldJump; // Trigger showing that the hero should jump
 
     // Ground Checks
     [SerializeField] private float playerBaseGroundRadius;
-    [SerializeField] LayerMask groundDef;
+    [SerializeField] private LayerMask groundDef;
 
+    // Variables
+    private float hrMovement;
 
+    // Temp Variables and Magic Numbers
+    [SerializeField] private float timeDilationFactor;
+    [SerializeField] private float activeCoolDown;
 
     // Start is called before the first frame update
     void Start()
     {
         // Establish ghost connection
-        //ghosts = FindObjectOfType<GhostProps>().GetComponent<GhostProps>();
         ghosts = FindObjectOfType<GhostProps>();
         Debug.Log(ghosts.PingMe());
 
@@ -45,6 +60,22 @@ public class PlayerController : MonoBehaviour
         heroTransform = GetComponent<Transform>();
         heroRigidBody = GetComponent<Rigidbody2D>();
 
+        // SetTimers
+        ActiveTimers = new float[3];
+        CoolDownTimers = new float[3];
+        for (int count = 0; count < 3; count++)
+        {
+            if (count == heroState)
+            {
+                ActiveTimers[count] = ghosts.hrMaxTimers[count];
+                CoolDownTimers[count] = 0;
+            }
+            else
+            {
+                ActiveTimers[count] = 0;
+                CoolDownTimers[count] = ghosts.hrMaxTimers[count];
+            }
+        }
         // Set default hero state
         heroState = 0;
 
@@ -57,22 +88,119 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
 
-    void Update()
+    void FixedUpdate()
     {
+        Time.timeScale = timeDilationFactor;
+        UpdatePositionChecks();
         HandleInput();
+        HandleMovement();
+        UpdateTimers();
+        // TODO : CHECK FOR TIMEOUTS
+        CheckForTimeOuts();
 
+        ResetValues();
+    }
+
+    private void CheckForTimeOuts()
+    {
+        if (ActiveTimers[heroState] == 0)
+        {
+            int newState = NextState();
+            if (CoolDownTimers[newState] >= 1)
+            {
+                heroState = newState;
+                UpdateHeroState();
+                SwapTimers();
+            }
+            else
+            {
+                newState = NextState();
+                if (CoolDownTimers[newState] >= 1)
+                {
+                    heroState = newState;
+                    UpdateHeroState();
+                    SwapTimers();
+                }
+                else
+                {
+                    // TODO Pause game
+                    Debug.Log(heroName + " IZ DED!");
+                    Time.timeScale = 0.0000001f;
+                }
+            }
+        }
+    }
+
+    private int NextState()
+    {
+        int newState;
+        if (heroState + 1 > 2)
+        {
+            newState = 0;
+        }
+        else
+        {
+            newState = heroState + 1;
+        }
+        return newState;
+    }
+
+    private void UpdateTimers()
+    {
+        timerDisplay.text = "";
+        string heroTimerMarker = "";
+
+        for (int count = 0; count < 3; count++)
+        {
+            if (count == heroState)
+            {
+                heroTimerMarker = "*";
+                ActiveTimers[count] = Math.Max((ActiveTimers[count] - (ghosts.hrActiveTempo[count] * Time.deltaTime)), 0);
+                CoolDownTimers[count] = Math.Min((CoolDownTimers[count] + (ghosts.hrCoolDownTempo[count] * Time.deltaTime * activeCoolDown)), ghosts.hrMaxTimers[count]);
+            }
+            else
+            {
+                // TODO : WARNING MAGIC NUMBERS
+                heroTimerMarker = ".";
+                CoolDownTimers[count] = Math.Min((CoolDownTimers[count] + (ghosts.hrCoolDownTempo[count] * Time.deltaTime)), ghosts.hrMaxTimers[count]);
+            }
+
+            // Update Display
+            timerDisplay.text += heroTimerMarker + " "  + ghosts.hrNames[count] + ": " + (int)ActiveTimers[count] + " (" + (int)CoolDownTimers[count] + ")\n";
+        }
+    }
+
+    private void ResetValues()
+    {
+    }
+
+    private bool IsOnGround()
+    {
+        if (heroRigidBody.velocity.y == 0)
+        {
+            foreach (Transform groundPoint in playerBase)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(groundPoint.position, playerBaseGroundRadius, groundDef);
+                for (int count = 0; count < colliders.Length; count++)
+                {
+                    if (colliders[count].gameObject != gameObject);
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 
     private void UpdatePositionChecks()
     {
-        // TODO : Check here: https://www.youtube.com/watch?v=05TCTrpGB-4&index=9&list=PLX-uZVK_0K_6VXcSajfFbXDXndb6AdBLO
-        midAir = true;
-        Collider2D baseCollider = Physics2D.OverlapCircle(playerBase.position, playerBaseGroundRadius, groundDef);
-        if (baseCollider.gameObject != gameObject)
+        onGround = IsOnGround();
+        if (onGround)
         {
-            Debug.Log("TouchDown!");
-            midAir = false;
+            didDoubleJump = false;
         }
+        shouldJump = false;
     }
 
     private void UpdateHeroState()
@@ -89,20 +217,49 @@ public class PlayerController : MonoBehaviour
     private void HandleInput()
     {
         HandleHeroStateChange();
-        HandleMovement();
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("UP");
+            shouldJump = true;
+        }
+
+        // Move Horizontal
+        hrMovement = Input.GetAxis("Horizontal");
     }
 
     private void HandleMovement()
     {
-        //// TEMPORARY MOVEMENT MECHANIC
-        //var heroHMv = Input.GetAxis("Horizontal");
-        //var heroVMv = Input.GetAxis("Vertical");
-        //if (heroHMv != 0 || heroVMv != 0)
-        //{
-        //    heroTransform.Translate(heroHMv * heroMoveSpeed * Time.deltaTime, heroVMv * heroMoveSpeed* Time.deltaTime, heroTransform.position.z);
-        //}
+        // Perform Jump
+        if ((onGround && shouldJump) || (heroDoubleJump && shouldJump && !didDoubleJump)) 
+        {
+            Debug.Log("Jumping");
+            if (!onGround)
+            {
+                didDoubleJump = true;
+            }
+            onGround = false;
+            heroRigidBody.AddForce(new Vector2(0, heroJumpFactor * tmpJumpFactor));
+            shouldJump = false;
+        }
 
-        // TODO : Jump Attempt
+        // Horizontal movement
+        if (onGround)
+        {
+            if (hrMovement > 0)
+            {
+                Debug.Log("Right!");
+            }
+            else if (hrMovement < 0)
+            {
+                Debug.Log("Left!");
+            }
+            {
+
+            }
+            heroRigidBody.velocity = new Vector2(hrMovement * heroMoveSpeed, heroRigidBody.velocity.y);
+        }
     }
 
     private void HandleHeroStateChange()
@@ -110,32 +267,50 @@ public class PlayerController : MonoBehaviour
         // Cycle Hero
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            if (heroState + 1 > 2)
-            {
-                heroState = 0;
-            }
-            else
-            {
-                heroState++;
-            }
+            SwapTimers();
+            heroState = NextState();
             UpdateHeroState();
+            SwapTimers();
         }
 
         // Change Hero Directly
         if (heroState != 0 && (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)))
         {
+            SwapTimers();
             heroState = 0;
             UpdateHeroState();
+            SwapTimers();
         }
         if (heroState != 1 && (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)))
         {
+            SwapTimers();
             heroState = 1;
             UpdateHeroState();
+            SwapTimers();
         }
         if (heroState != 2 && (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)))
         {
+            SwapTimers();
             heroState = 2;
             UpdateHeroState();
+            SwapTimers();
         }
+    }
+
+    private void SwapTimers()
+    {
+        // Activation or Deactivation
+        // Swap Active and CoolDown Timers for CURRENT hero
+
+        //// DEPRECATED
+        ////float newActive = ghosts.hrMaxTimers[heroState] - CoolDownTimers[heroState];
+        //float newActive = CoolDownTimers[heroState];
+        //float newCoolDown = ghosts.hrMaxTimers[heroState] - ActiveTimers[heroState];
+
+        float newActive = CoolDownTimers[heroState];
+        float newCoolDown = ActiveTimers[heroState];
+        //Debug.Log("New Active: " + newActive + " / New CoolDown: " + newCoolDown);
+        ActiveTimers[heroState] = newActive;
+        CoolDownTimers[heroState] = newCoolDown;
     }
 }
