5a5928f6498296b799045affb1e93f5f12469ca0
MossGoblin <hugolabs@gmail.com>
2019-03-21 19:18:19 +0200

    [progress] spawning stabilized; added  life span for enemies to test spawning in pre-attack conditions; TODO: auto-spawn, spawnPoint timers, initial spawn seeding

--- Assets/Scripts/enemyController.cs
+++ Assets/Scripts/enemyController.cs
@@ -1,91 +1,34 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemyController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
-    public GameObject bullet;
-    public Transform shootingPoint;
-    public Transform target;
-    [SerializeField] private timeController master;
-    public float bulletInterval;
-    public float bulletSpeed;
-    public float bulletLifeSpan;
-    public float damage;
-    public int identity;
-    public int totalLifePoints;
-    public float timeToken;
-    private int lifePoints;
+    [SerializeField] SpawnMaster spawnMaster;
 
-    private float bulletTimer;
+    [SerializeField] public float timeLeft;
+
+    public int spawnPointIndex;
+    public int selfType;
 
     // Start is called before the first frame update
     void Start()
     {
-        lifePoints = totalLifePoints;
+        // set time left
+        timeLeft = 5f;
+
+        // get conductor
+        spawnMaster = GameObject.FindObjectOfType<SpawnMaster>();
     }
 
-// Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
     {
-        bulletTimer += Time.deltaTime;
-        if (bulletTimer >= bulletInterval)
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
         {
-            bulletTimer = 0;
-            Shoot();
+            spawnMaster.RemoveEnemy(gameObject, selfType, spawnPointIndex);
+            Destroy(gameObject);
         }
-        SwapFacing();
-    }
-
-    private void SwapFacing()
-    {
-        // get current scale
-        Vector3 newScale = transform.localScale;
-        // get direction
-        Vector2 direction = target.transform.position - transform.position;
-        direction.Normalize();
-        // if the current scale and the direction are with opposite signs
-        if (FacingOpposite(direction.x, newScale.x))
-        {
-            // adjust local scale
-            newScale.x *= -1;
-            transform.localScale = newScale;
-        }
-    }
-
-    private bool FacingOpposite(float valueOne, float valueTwo)
-    {
-        return !((valueOne >= 0 && valueTwo >= 0) || (valueOne < 0 && valueTwo < 0));
-    }
-
-    private void Shoot()
-    {
-        Vector2 direction = target.transform.position - transform.position;
-        direction.Normalize();
-        //Debug.Log("direction: " + direction);
-        GameObject newBullet = Instantiate(bullet, shootingPoint.transform.position, shootingPoint.transform.rotation);
-        //Debug.Log("bulletspawn");
-        newBullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
-        newBullet.GetComponent<enemyBulletAI>().dmg = damage;
-        newBullet.GetComponent<enemyBulletAI>().lifespan = bulletLifeSpan;
-
-        //Debug.Log("bullet velocity: " + direction * bulletSpeed + " / " + newBullet.GetComponent<Rigidbody2D>().velocity);
-    }
-
-    public void TakeDamage(int dmgTaken)
-    {
-        Debug.Log("hit to " + lifePoints + " / " + (lifePoints - dmgTaken));
-        lifePoints -= dmgTaken;
-        if (lifePoints <= 0)
-        {
-            Die();
-        }
-    }
-
-    private void Die()
-    {
-        master.ReceiveTimeBonus(identity, 3);
-        Destroy(gameObject);
     }
 }
